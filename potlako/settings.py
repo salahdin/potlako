3b9ea21bbc4e7805ed0e3eb6146c3a0b97d3bd4b
tumiemosweu <imosweuk@gmail.com>
2020-04-03 09:31:41 +0200

    update settings file

--- potlako/settings.py
+++ potlako/settings.py
@@ -177,16 +177,13 @@ LANGUAGES = (
     ('kck', 'Ikalanga'),
 )
 
-en_formats.DATETIME_FORMAT = "d-m-Y H:i:s"
-en_formats.DATE_FORMAT = "d-m-Y"
-
 DATE_INPUT_FORMATS = ['%d-%m-%Y']
 
 TIME_ZONE = 'Africa/Gaborone'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
 
